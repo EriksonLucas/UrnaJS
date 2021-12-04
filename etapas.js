be7f659fc00b14e9523b9023297fb141faf5f11a
Lucas Erikson <73944190+EriksonLucas@users.noreply.github.com>
2021-12-04 20:03:15 -0300

    adicionando códigos

---
+++ etapas.js
@@ -0,0 +1,86 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Sakura Haruno',
+                partido: 'AAAA',
+                fotos: [
+                    {url: 'candidato1.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Kakashi Hatake',
+                partido: 'BBBB',
+                fotos: [
+                    {url: 'candidato2.jpg',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Rock Lee',
+                partido: 'CCCC',
+                fotos: [
+                    {url: 'candidato3.jpg',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '44444',
+                nome: 'Kisame Hoshigaki',
+                partido: 'Akatsuki',
+                fotos: [
+                    {url: 'candidato4.jpg',legenda: 'Vereador',small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Hashirama Senju',
+                partido: 'AAAA',
+                vice: 'Madara',
+                fotos: [
+                    {url: 'prefeito1.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'vice1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Hiruzen Sarutobi',
+                partido: 'BBBB',
+                vice: 'Jiraiya',
+                fotos: [
+                    {url: 'prefeito2.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'vice2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '33',
+                nome: 'Naruto Uzumaki',
+                partido: 'CCCC',
+                vice: 'Sasuke',
+                fotos: [
+                    {url: 'prefeito3.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'vice3.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'Itachi Uchiha',
+                partido: 'Akatsuki',
+                vice: 'Nagato',
+                fotos: [
+                    {url: 'prefeito4.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'vice4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
